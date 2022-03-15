6cd90b96cdeef7957f31280c4583659112e97772
David <4292@holbertonschool.com>
2022-03-15 14:34:32 +0000

    Adding header file

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+* _putchar 
+* specifier - checks for conversion specifier
+* validator - validates what specifier it is, return 1 for true, 0 for false
+* function pointer - points to appropiate function based on specifier
+*/
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct convert
+{
+	char *data;
+	int (*f)(va_list ptr);
+} conv;
+
+int _printf(const char *format, ...);
+int print_char(va_list ptr);
+int print_int(va_list ptr);
+int print_string(va_list ptr);
+int print_percent(va_list ptr);
+char *convert(unsigned int num, int base);
+
+#endif 
